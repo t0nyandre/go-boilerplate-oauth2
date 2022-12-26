e91b1c2a271ce52f4c046123c18c47b16157b1e5
t0nyandre <post@tonyandre.co>
2022-12-26 13:48:41 +0100

    Initial commit a hello world example, custom logger and godotenv

---
+++ cmd/boilerplate/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"github.com/t0nyandre/go-boilerplate-oauth2/pkg/logger"
+)
+
+func init() {
+	if err := godotenv.Load("config/env/.env"); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
+
+func main() {
+	router := chi.NewRouter()
+	logger := logger.NewLogger()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", "World")
+	})
+
+	logger.Infow("Server successfully up and running", "host", os.Getenv("APP_HOST"), "port", os.Getenv("APP_PORT"))
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%s", os.Getenv("APP_HOST"), os.Getenv("APP_PORT")), router); err != nil {
+		logger.Fatalw("Server failed to start", "error", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/t0nyandre/go-boilerplate-oauth2
+
+go 1.19
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/joho/godotenv v1.4.0
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+)
