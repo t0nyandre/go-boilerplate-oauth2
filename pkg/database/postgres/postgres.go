aa93fc83feabcdcdbdc4d0447e2987cc43b0c335
t0nyandre <post@tonyandre.co>
2023-01-16 00:33:52 +0100

    Added better errorhandling with db.Ping and a retry connection feature

--- pkg/database/postgres/postgres.go
+++ pkg/database/postgres/postgres.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -10,18 +11,29 @@ import (
 )
 
 func NewPostgres(logger *zap.SugaredLogger) (*sqlx.DB, error) {
-	db, err := sqlx.Open("postgres",
+	db, _ := sqlx.Open("postgres",
 		fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
 			os.Getenv("POSTGRES_USER"),
 			os.Getenv("POSTGRES_PASSWORD"),
 			os.Getenv("POSTGRES_DB"),
 			os.Getenv("POSTGRES_SSLMODE")))
-	if err != nil {
-		return nil, err
+
+	if err := db.DB.Ping(); err != nil {
+		logger.Warnw(
+			"Retrying database connection in 5 seconds",
+			"appName", os.Getenv("APP_NAME"),
+			"user", os.Getenv("POSTGRES_USER"),
+			"database", os.Getenv("POSTGRES_DB"),
+			"error", err,
+		)
+		time.Sleep(time.Duration(5) * time.Second)
+		return NewPostgres(logger)
 	}
 
-	logger.Infow("Successfully connected to database",
-		"database", os.Getenv("POSTGRES_DB"))
+	logger.Infow(
+		"Successfully connected to database",
+		"database", os.Getenv("POSTGRES_DB"),
+	)
 
 	return db, nil
 }
